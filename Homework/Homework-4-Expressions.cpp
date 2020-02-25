e3a2826b21f4d99cfca4d27b28861a091a2fd4ce
AChen24562 <alexchen24562@gmail.com>
2020-02-24 19:03:39 -0500

    Homework 4 update

--- Homework/Homework-4-Expressions.cpp
+++ Homework/Homework-4-Expressions.cpp
@@ -6,11 +6,34 @@ using namespace std;
 
 int main(){
 
-cout << "Transportation Center" << endl
-  << "-------------------------------";
+// Bus program
+  cout << "Transportation Center" << endl
+  << "-------------------------------" << endl;
+  // Amount of people per bus
+  int bus_cap = 55;
+  int passengers;
+  cout << "How many passengers are there? ";
+  cin >> passengers;
+  //  Determining the amount of people per bus
+  int buses = passengers/bus_cap;
+  cout << endl << "There will be " << buses << " buses with "
+       << bus_cap << " people on each bus." << endl;
+  // Determining the amount of people will be in vans
+  int remaining = passengers%bus_cap;
+  cout << "With " << remaining << " riding in vans. Have a nice trip!"
+       << endl << endl;
 
 
-
+// Digit extraction problem
+int digit;
+cout << "Digit Extraction" << endl;
+cout << "--------------------------------" << endl;
+cout << "Enter a 3-Digit Integer: ";
+cin >> digit;
+cout << endl;
+cout << digit % 10
+     << endl << digit / 10 % 10
+     << endl << digit / 100;
 
 
 
