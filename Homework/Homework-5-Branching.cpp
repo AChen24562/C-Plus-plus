99e958eb050ee404e1f2da64b702ec3ee78d546b
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2020-03-02 21:03:01 -0500

    Update Homework-5-Branching.cpp

--- Homework/Homework-5-Branching.cpp
+++ Homework/Homework-5-Branching.cpp
@@ -1,3 +1,8 @@
+// Alex Chen
+// Prof. J Sun
+// Homework 5 Branching
+
+
 #include <iostream>
 using namespace std;
 
@@ -9,17 +14,15 @@ cout << "Enter a second integer: ";
 cin >> b;
 cout << "Enter a possible factor of those two integers: ";
 cin >> c;
-
+ // Determining if a number is the factor of the other 2 numbers
 if (a % c == 0 && b % c == 0)
     cout << c << " is a common factor of " << a
          << " and " << b << endl;
-else if(a % c != 0 && b % c != 0)
-    cout << c << " is NOT a common factor of " << a
-        << " and " << b << endl;
-else if (c == 0)
-    cout << "Error";
+         else
+          cout << c << " is NOT a common factor of " << a
+          << " and " << b << endl;
 
-// Determining a character is lower or upper
+// Determining a character is lower, upper, punctuation, or number
 char letter;
 cout << "Enter a character: ";
 cin >> letter;
