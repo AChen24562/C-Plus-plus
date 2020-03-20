236ffa011225f1eacc7a97102d983ccb56599bcb
AChen24562 <alexchen24562@gmail.com>
2020-03-19 23:51:28 -0400

    week7

---
+++ Classwork/Week7-day2-part1.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+/*int row, col;*/ // #5
+
+
+
+// 5 make continue as comment
+/*
+do{
+cout << "Enter the number of rows (1-10): ";
+cin >> row;
+cout << "Enter the number of columns (1-10): ";
+cin >> col;
+cout << endl;
+
+if(row > 0 && row < 11 && col > 0 && col < 11)
+    break;
+else{
+  cout << "Invalid input. Please re-enter. \n\n";
+  continue;
+  }
+    cout << "Today is the first online class. " <<endl;
+}while(true);
+
+  for(int r=0; r < row; r++ )
+  {
+    for(int c=0; c<col; c++)
+        cout << "*" << "\t";
+        cout <<endl;
+  }
+
+cout <<endl;
+*/
+
+
+// 6 Request integer value 'n' and use loop to print all factors of 'n'
+/*int n;
+
+cout << "Enter a value for 'n': ";
+cin >> n;
+cout << endl;
+
+cout << "Factors: ";
+for(int i = 1; i <= n; i++){
+    if(n % i == 0)
+      cout << i << " ";
+}*/
+
+
+// 7 Determin if a number is a prime or not
+/*int n;
+cout << "Enter a number: ";
+cin >> n;
+int numFactors =0;
+
+cout << endl;
+    for(int i =2; i <n; i++){
+        if(n%i == 0 )
+        numFactors++;
+     }
+if(numFactors > 0){
+  cout << n << " is not a prime number." << endl;
+}
+else{
+  cout << n << " is a prime number. "<< endl;
+}*/
+
+// bool method for 7
+int number;
+bool isPrime = true;
+
+cout << "Enter a number: ";
+cin >>number;
+
+for(int i=2; i<number; i++){
+  if(number %1 ==0){
+      isPrime =false;
+      break;
+  }
+}
+if(isPrime)
+    cout << number << " is a prime number. " << endl;
+
+else
+    cout << number << " is not a prime number. " << endl;
+
+
+
+  return 0;
+}
