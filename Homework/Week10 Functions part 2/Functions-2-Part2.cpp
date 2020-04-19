8c12881a295f8fa98ef1ae87f79e14a2c929d775
AChen24562 <alexchen24562@gmail.com>
2020-04-18 21:02:38 -0400

    homework10

--- Homework/Week10 Functions part 2/Functions-2-Part2.cpp
+++ Homework/Week10 Functions part 2/Functions-2-Part2.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+
 #include <cmath>
 using namespace std;
 
 
+
 int main(){
-int x;
-x = rand();
+srand(time(NULL));
 
-
+cout << "\nRandom sequence between 0 and 6:" << endl;
+for(int i=0; i < 1; i++)
+    cout << rand() % 7 << " ";
+cout << endl << endl;
 
   return 0;
 }
--- Homework/Week10 Functions part 2/Functions-2.cpp
+++ Homework/Week10 Functions part 2/Functions-2.cpp
@@ -1,4 +1,11 @@
-#include <iostream>
+//Prof. Sun
+//Homework Week 10
+//Alex Chen
+
+
+
+//Question 1
+/*#include <iostream>
 #include <cmath>
 using namespace std;
 
@@ -33,4 +40,54 @@ int main(){
 
 
   return 0;
-}
+}*/
+
+
+//Question 2
+/*#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+  void roll(){
+    srand(time(NULL));
+
+    for(int i=0; i < 3; i++)
+        cout << "Roll <6>: " << rand() % 6 + 1 << endl;
+  }
+
+  void roll(int num, int num2, int num3){
+    srand(time(NULL));
+
+    for(int i=0; i < 1; i++)
+        cout << "Roll <" << num << ">: " << rand() % num + 1 << endl;
+        cout << "Roll <" << num2 << ">: " << rand() % num2 + 1 << endl;
+        cout << "Roll <" << num3 << ">: " << rand() % num3 + 1 << endl;
+  }
+
+  void roll(int n, int r){
+    srand(time(NULL));
+    int counter = 0;
+
+    for(int i=0; i < r; i++){
+      counter += rand() % n + 1;
+      }
+      cout << "Roll <" << n << "d"<< r << ">: " << counter << endl;
+
+  }
+
+
+
+int main(){
+
+roll();
+cout << endl << endl;
+roll(10, 20, 30);
+cout << endl << endl;
+roll(3, 7);
+roll(3, 8);
+roll(5, 10);
+
+
+return 0;
+}*/
