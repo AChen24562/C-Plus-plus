605bb23aca1010e3cc70d9a6ee4eb5bbfe333a33
AChen24562 <alexchen24562@gmail.com>
2020-04-20 01:55:53 -0400

    Homework10 update

--- Homework/Week10 Functions part 2/Week10-Functions-2-Part2.cpp
+++ Homework/Week10 Functions part 2/Week10-Functions-2-Part2.cpp
@@ -2,49 +2,8 @@
 //Homework Week 10
 //Alex Chen
 
-
-
-//Question 1
-/*#include <iostream>
-#include <cmath>
-using namespace std;
-
-double sqrtRounded(double num){
-
-  double result = sqrt(num);
-  int rounded = round(result);
-  return rounded;
-}
-
-int main(){
-  double num;
-  do{
-    cout << "Enter a positive number <Or Enter a ZERO to exit>: ";
-    cin >> num;
-  if(num >-1){
-    if(num == 0){
-        cout << num << " is entered. Exiting Program." << endl
-             << "---------------------------------";
-        exit(0);
-    }
-    int rounded = sqrtRounded(num);
-    cout << "The square root of " << num << " rounded to the nearest integer is "
-         << rounded << endl << endl;
-
-  }
-    else
-        cout << num << " is an invalid input. Please re-enter. "<< endl << endl;
-
-  }while(true);
-
-
-
-  return 0;
-}*/
-
-
 //Question 2
-/*#include <iostream>
+#include <iostream>
 #include <cmath>
 #include <ctime>
 using namespace std;
@@ -90,4 +49,4 @@ roll(5, 10);
 
 
 return 0;
-}*/
+}
