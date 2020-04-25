c939ec45b866f3c6b76099dc72870219a0eb7243
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2020-04-25 12:54:15 -0400

    ??

---
+++ 100plus.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//5050
+int main(){
+  int final =0;
+
+  for(int i =40; i <=100; i++){
+    final += i;
+    cout << final << " ";
+  }
+  cout << endl << endl << final;
+}
