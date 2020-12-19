03b1b7bc9534b5e951220c3482626ae314439e44
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2020-12-19 18:42:36 -0500

    review for next semester object oriented

---
+++ Intro To C++/After_Semester_Practice/Exam1A_1_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+
+// #1
+string exam = "ET575ExamOne";
+cout << "#1" << endl <<exam << endl;
+
+// #2
+string greeting = "Hello Wednesday";
+cout << greeting[9] << endl;
+
+for(int i =0; i < greeting.length(); i ++){
+  if(greeting[i] == 'n'){
+    cout << greeting[i] << " is in position " << i;
+  }
+}
+
+
+  return 0;
+}
