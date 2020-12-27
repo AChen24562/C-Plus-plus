1950950f7a736a367b6d4efaef8403eaf5ecdda0
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2020-12-27 06:15:24 -0500

    asd

---
+++ Intro To C++/After_Semester_Practice/Arrays/lesson_1_1D_array.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <ctime>
+
+using namespace std;
+
+// Making an array
+void populate_ar(int array[], const int SIZE){
+  for(int i = 0; i < SIZE; i ++){
+    array[i] = i;
+  }
+}
+
+// Displaying array
+void display(int array[], const int SIZE){
+  for(int i = 0; i <SIZE; i++){
+    cout << array[i] << " ";
+  }
+}
+
+// Recursive Sum
+int sumArray(int array[], int counter, int sum){
+  if(counter == 0){
+    return sum;
+  }
+  else{
+    counter --;
+    sum += array[counter];
+    return sumArray(array, counter, sum);
+  }
+}
+
+
+int getIndex(const int array[], const int size, int element){
+  for(int i = 0; i <= size; i ++){
+    if(array[i] == element){
+      return i;
+    }
+
+  }
+  return -1;
+}
+
+
+int main(){
+
+  // Init array
+  const int SIZE = 5;
+  int array[SIZE];
+
+  populate_ar(array, SIZE);
+
+  display(array, SIZE);
+  cout << endl;
+
+  int sum = 0;
+  cout <<  sumArray(array, SIZE, sum);
+
+  int element = 3;
+  cout << endl << "Index of " << element << " is " << getIndex(array, SIZE, element);
+
+
+  return 0;
+
+}
