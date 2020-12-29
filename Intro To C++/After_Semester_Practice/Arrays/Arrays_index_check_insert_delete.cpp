3b0d91b0ed7cabfe69d1eacfbb30a61d8c4d666a
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2020-12-29 05:49:09 -0500

    Update Arrays_index_check_insert_delete.cpp

--- Intro To C++/After_Semester_Practice/Arrays/Arrays_index_check_insert_delete.cpp
+++ Intro To C++/After_Semester_Practice/Arrays/Arrays_index_check_insert_delete.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
 using namespace std;
 
-void populate(int array[], const int SIZE);
-void display(int array[], const int SIZE);
-int getIndex(const int array[], const int& AMOUNT, int element);
-void check_index(int array[], int& AMOUNT, int element);
-
-int main(){
-  const int SIZE = 10;
-  int array[SIZE];
-
-  int amount = 5;
-  populate(array, amount);
-  display(array, amount);
-
-  int element = 3;
-  int index = getIndex(array, amount, element);
-  //cout << endl << printf("%d is the index of %d\n", index, element) << endl << endl << "asd";
-  check_index(array, amount, element);
-
-
-  return 0;
-}
-
-
 // Functions
 void populate(int array[], const int SIZE){
   for(int i = 0; i < SIZE; i++){
@@ -37,6 +14,7 @@ void display(int array[], const int SIZE){
   }
 }
 
+// Element's index
 int getIndex(const int array[], const int& AMOUNT, int element){
   for(int i =0; i< AMOUNT; i ++){
     if(array[i] == element){
@@ -47,12 +25,49 @@ int getIndex(const int array[], const int& AMOUNT, int element){
 }
 
 
+// Check if element is within array
 void check_index(int array[], int& AMOUNT, int element){
   int index = getIndex(array, AMOUNT, element);
   if(index < 0){
-    cout << printf("\n%d is not within the array\n", element);
+    printf("\n%d is not within the array\n", element);
   }
   else{
     printf("\n%d exists in the array\n", element);
   }
+}
+
+
+void removeElement(int array[], int& amount, int element){
+  int index = getIndex(array, amount, element);
+  if(index < 0){
+    cout << "Invalid" << endl;
+    return;
+  }
+  amount --;
+  for(int i = index; i < amount; i++){
+    array[i] = array[i+1];
+  }
+
+}
+
+int main(){
+  const int SIZE = 10;
+  int array[SIZE];
+
+  int amount = 5;
+  populate(array, amount);
+  display(array, amount);
+
+  int element = 3;
+  int index = getIndex(array, amount, element);
+  printf("\n%d index is %d\n", element, index );
+
+  int incorrect_element = 20;
+  check_index(array, amount, element);
+  check_index(array,amount, incorrect_element);
+  removeElement(array, amount, element);
+  display(array, amount);
+
+
+  return 0;
 }
