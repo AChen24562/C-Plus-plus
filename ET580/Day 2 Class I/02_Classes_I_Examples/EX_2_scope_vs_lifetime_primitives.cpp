5ec86b7a2255492c309a857ebe94f179a5587bc1
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2021-02-08 22:25:37 -0500

    scope and time

--- ET580/Day 2 Class I/02_Classes_I_Examples/EX_2_scope_vs_lifetime_primitives.cpp
+++ ET580/Day 2 Class I/02_Classes_I_Examples/EX_2_scope_vs_lifetime_primitives.cpp
@@ -3,12 +3,17 @@
 using namespace std;
 
 // pass and return by value
+// Prints different memory location
+// the scope of val is local to f1
 int f1(int val) {
     cout << "val            " << &val << endl;
     return val;
 }
 
 // pass and return by reference
+// prints the same memory location
+// r is just an alias for i's memory location, therefore the scope of r is
+// local to f2
 int& f2(int &r) {
     cout << "r              " << &r << endl;
     return r;
@@ -23,7 +28,7 @@ int main() {
     i = f1(i);
     cout << "i              " << &i << "\n\n";
 
-    i = f2(i);
+    i = f2(i);  // call f2 and send i by reference to new variable r
     cout << "i              " << &i << "\n\n";
 
     cout << endl;
